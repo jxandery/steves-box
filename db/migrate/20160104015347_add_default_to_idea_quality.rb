f55bbfc64452c4a89cba66bb893657563d7353ef
Jack Yeh <jack.yeh@gmail.com>
2016-01-03 19:08:17 -0700

    add validations for idea model

---
+++ db/migrate/20160104015347_add_default_to_idea_quality.rb
@@ -0,0 +1,5 @@
+class AddDefaultToIdeaQuality < ActiveRecord::Migration
+  def change
+    change_column :ideas, :quality, :string, default: 'swill'
+  end
+end
---
+++ test/models/idea_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class IdeaTest < ActiveSupport::TestCase
+  test 'should have a quality that defaults to swill' do
+    idea = Idea.new
+    assert_equal('swill', idea.quality)
+  end
+
+  test 'it should be invalid without a title or body' do
+    idea_without_title_or_body = Idea.new
+    refute(idea_without_title_or_body.valid?)
+  end
+
+  test 'it should be invalid without a title' do
+    idea = Idea.new(body: 'body')
+    refute(idea.valid?)
+  end
+
+  test 'it should ve invalid without a body' do
+    idea = Idea.new(title: 'title')
+    refute(idea.valid?)
+  end
+
+  test 'it is valid with a title and body' do
+    idea_with_title_and_body = Idea.new(title: 'title', body: 'body')
+    assert(idea_with_title_and_body.valid?)
+  end
+end
