43e8c201a49ae09d0a68ef4d561fa9f4f3e17dc3
Jack Yeh <jack.yeh@gmail.com>
2016-01-03 19:47:35 -0700

    create/pass test: #index returns the correct number of ideas and ideas contain the correct properties

--- test/controllers/api/v1/ideas_controller_test.rb
+++ test/controllers/api/v1/ideas_controller_test.rb
@@ -8,7 +8,22 @@ class Api::V1::IdeasControllerTest < ActionController::TestCase
 
   test 'index returns an array of records' do
     get :index, format: :json
-
     assert_kind_of Array, json_response
   end
+
+  test '#index returns the correct number of ideas' do
+    get :index, format: :json
+    assert_equal Idea.count, json_response.count
+  end
+
+  test '#index contains ideas witht he correct properties' do
+    get :index, format: :json
+
+    json_response.each do |idea|
+      assert idea['title']
+      assert idea['body']
+      assert idea['quality']
+    end
+  end
+
 end
