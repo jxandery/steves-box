26b6cea8eb27aa0cae46ff79db30dab8743f6b37
Jack Yeh <jack.yeh@gmail.com>
2016-01-10 22:54:27 -0700

    create/pass test: <h1> tag has the content Idea Box

--- test/integration/basic_template_test.rb
+++ test/integration/basic_template_test.rb
@@ -6,4 +6,9 @@ class BasicTemplateTest < ActionDispatch::IntegrationTest
     visit root_path
     assert_equal 200, page.status_code
   end
+
+  test 'it has an <h1> tag with the content Idea Box' do
+    visit root_path
+    assert page.find('h1').has_content? 'Idea Box'
+  end
 end
